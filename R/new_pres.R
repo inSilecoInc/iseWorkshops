7afb307387abc40d76ad219c46d8cea7dfc05fd9
Kevin Cazelles <kevin.cazelles@insileco.io>
2022-10-09 16:21:20 -0400

    Review workflows

--- R/new_pres.R
+++ R/new_pres.R
@@ -1,23 +1,23 @@
 #' Shortcuts for interactive functions.
 #'
-#' @param title presentation title. 
+#' @param title presentation title.
 #' @param date presentation date.
 #' @param author author name.
-#' @param pres_name name of the folder that includes the presentation.  
+#' @param pres_name name of the folder that includes the presentation.
 #' @param overwrite a logical. Overwrite files if they exist (see [fs::dir_copy()]).
 #'
-#' @export 
+#' @export
 
 new_pres <- function(title = "Cool title", date = Sys.Date(), author = "inSileco Tea", pres_name = "newpres", overwrite = FALSE) {
   dir_copy(
-    path_package(package = "iseWorkshops", "templates/newpres"), ".", 
+    path_package(package = "iseWorkshops", "templates/newpres"), ".",
     overwrite = overwrite
   )
   writeLines(
     whisker::whisker.render(
-      readLines("newpres/index.Rmd"), 
-      , data = list(title = title, date = date, author = author)
-    ), 
+      readLines("newpres/index.Rmd"), ,
+      data = list(title = title, date = date, author = author)
+    ),
     "newpres/index.Rmd"
   )
   cli::cli_alert_info("folder 'newpres' created")
@@ -27,6 +27,6 @@ new_pres <- function(title = "Cool title", date = Sys.Date(), author = "inSileco
   #
   cli::cli_alert_info("Rendering presentation and opening it")
   openHTML("newpres/index.html")
-  
+
   invisible(NULL)
 }
