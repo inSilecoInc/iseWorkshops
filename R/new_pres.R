061ba0431d72df812b417d5a5893ca87683538f2
Kevin Cazelles <kevin.cazelles@insileco.io>
2023-11-10 13:09:33 -0500

    :bookmark: v0.2.0

--- R/new_pres.R
+++ R/new_pres.R
@@ -15,32 +15,28 @@ new_pres <- function(title = "Your title", date = Sys.Date(),
                      author = "inSileco Team", pres_name = "newpres",
                      overwrite = FALSE, output_dir = NULL) {
   if (is.null(output_dir)) {
-    output_dir <- fs::dir_create(sprintf("pres_%s", Sys.Date())) |>
+    output_dir <- sprintf("pres_%s", Sys.Date()) |>
       as.character()
   }
   fs::dir_copy(
-    fs::path_package(package = "iseWorkshops", "templates/newpres"), output_dir,
+    fs::path_package(package = "iseWorkshops", "templates/newpres"), 
+    output_dir,
     overwrite = overwrite
   )
+
   rmd <- file.path(output_dir, "index.Rmd")
   writeLines(
     whisker::whisker.render(
-      readLines(
-        fs::path_package(
-          package = "iseWorkshops",
-          file.path("templates", "newpres", "index.Rmd")
-        )
-      ),
-        data = list(title = title, date = date, author = author)
-      ),
-      rmd
+      readLines(rmd), 
+      data = list(title = title, date = date, author = author)
+    ),
+    rmd
   )
-  cli::cli_alert_info("folder 'newpres' created")
+  cli::cli_alert_info("Presentation files available in {output_dir}.")
+
   #
   cli::cli_alert_info("Rendering presentation")
-  rmarkdown::render(rmd)
-  #
-  cli::cli_alert_info("Rendering presentation and opening it")
+  rmarkdown::render(rmd, output_dir = output_dir)
   openHTML(rmd |> fs::path_ext_set("html"))
 
   invisible(NULL)
